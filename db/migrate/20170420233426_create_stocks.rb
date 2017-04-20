7214b9e9fc2378f817e6ec85d61cc9c6b9bf47f4
jimwong1023 <jimwong1023@gmail.com>
2017-04-20 19:58:42 -0400

    first commit

---
+++ db/migrate/20170420233426_create_stocks.rb
@@ -0,0 +1,9 @@
+class CreateStocks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :stocks do |t|
+      t.string :ticker, null: false, unique: true
+      t.string :company_name, null: false
+      t.string :exchange, null: false
+    end
+  end
+end
