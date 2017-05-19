5478e9771c9cff1e41a282353292b0dd485558a6
jimwong1023 <jimwong1023@gmail.com>
2017-05-19 10:31:10 -0400

    style changes

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,7 @@
+class StaticPagesController < ApplicationController
+  def home_page
+  end
+
+  def research
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'home_page#show'
+  root to: 'static_pages#home_page'
+  get '/research', to: 'static_pages#research'
 
   resources :portfolios, only: [:show]
 end
